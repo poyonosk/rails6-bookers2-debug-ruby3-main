610f6e876e20290cba01c4fadce1e3d4b8a5d962
poyonosk <117244700+poyonosk@users.noreply.github.com>
2023-01-06 02:47:28 +0000

    [Finish]kadai4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,35 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  
+
   has_one_attached :profile_image
-  
+
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true, length: {minimum: 2, maximum: 20}
   validates :introduction, presence: false, length: {maximum: 50}
-  
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  has_many :follows, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :follows, source: :followed
+  has_many :followers, through: :follower, source: :follower
+
+  def follow(user)
+    follows.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    follows.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
